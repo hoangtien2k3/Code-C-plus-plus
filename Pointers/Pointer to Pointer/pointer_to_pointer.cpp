e96a794f450f87b81b3db26c98b4508fae0af88c
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-04-02 16:21:54 +0700

    Pointer to Pointer_02/04/2022

---
+++ Pointers/Pointer to Pointer/pointer_to_pointer.cpp
@@ -0,0 +1,39 @@
+
+#include<iostream>
+using namespace std;
+int main()	{
+
+	int value = 100;
+	int *ptr = &value;
+	int **p_to_p = &ptr;
+
+    // int **p_to_p = &ptr || int *ptr = &value || int value = 10
+
+    cout << endl;
+
+    cout << "Dia chi bien value: " << endl;
+    cout << &value << endl;
+    cout << ptr << endl;
+    cout << *(*&p_to_p) << endl;
+    cout << *p_to_p << endl << endl; // địa chỉ được giữ bởi ptr
+
+    cout << "Gia tri value: " << endl;
+    cout << value << endl;
+    cout << *&value << endl;
+    cout << *ptr << endl;
+    cout << *(*p_to_p) << endl << endl; // giá trị được giữ bởi ptr
+    
+
+    cout << "Dia chi ptr: " << endl;
+    cout << &ptr << endl;
+    cout << p_to_p << endl << endl; // địa chỉ của ptr
+
+    cout << "Dia chi p_to_p: " << endl;
+    cout << &*p_to_p << endl << endl;
+
+
+
+
+
+	return 0;
+}
