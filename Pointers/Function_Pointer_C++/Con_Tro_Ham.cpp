438d96a4c4d83c23d39147b01208a2afe52e91ef
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-04-08 08:30:26 +0700

    07/04/2022

--- Pointers/Function_Pointer_C++/Con_Tro_Ham.cpp
+++ Pointers/Function_Pointer_C++/Con_Tro_Ham.cpp
@@ -11,9 +11,6 @@
     Trong đó int là kiểu trả về của hàm foo, pFoo là tên con trỏ, và hàm foo không có tham số nên 
     phần trong ngoặc chúng ta bỏ trông
 
-
-
-
 */
 
 /*
@@ -38,14 +35,19 @@
     int (*funcPtr3)(int) = hoo; // okay
 
 */
+/*
+    ! Lưu ý, tham số mặc định của hàm không áp dụng được cho con trỏ hàm, 
+    ! vì tham số mặc định được compiler xác định tại thời điểm compile chương trình, 
+    ! còn con trỏ hàm được sử dụng tại thời điểm chương trình đang chạy.
 
+*/
 
 #include<iostream>
 #include<string>
 #include<cmath>
 using namespace std;
 
-void swapValue(int &value1, int &value2) 
+void swapValue(int &value1, int &value2)
 {
 	int temp = value1;
 	value1 = value2;
@@ -54,14 +56,70 @@ void swapValue(int &value1, int &value2)
 
 int main()
 {
-	void(*pSwap) (int &, int &) = swapValue; //! Lưu ý, khi cần lấy địa chỉ của hàm, chúng ta chỉ sử dụng duy nhất tên hàm, không đặt thêm cặp dấu ngoặc vào.
+    int a = 1, b = 5;
+	void(*pSwap) (int &, int &) = swapValue;
 
-	cout << pSwap << endl;
-	cout << swapValue << endl;
-	
+	cout << "Before: " << a << " " << b << endl;
+	(*pSwap)(a, b);
+	cout << "After:  " << a << " " << b << endl;
+
 	return 0;
 }
 
 
 
+
+
+///! ////////////////////////////////////////
+
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+bool ascending(int left, int right)
+{
+	return left > right;
+}
+
+bool descending(int left, int right)
+{
+	return left < right;
+}
+
+void selectionSort(int *arr, int length, bool (*comparisonFunc)(int, int))
+{
+	for (int i_start = 0; i_start < length; i_start++)
+	{
+		int minIndex = i_start;
+
+		for (int i_current = i_start + 1; i_current < length; i_current++)
+		{
+			if (comparisonFunc(arr[minIndex], arr[i_current])) // use function pointer as ascending or descending function
+			{
+				minIndex = i_current;
+			}
+		}
+
+		swap(arr[i_start], arr[minIndex]); // std::swap
+	}
+}
+
+int main()
+{
+	int arr[] = { 1, 4, 2, 3, 6, 5, 8, 9, 7 };
+	int length = sizeof(arr) / sizeof(int);
+
+	selectionSort(arr, length, descending);
+    for (int i=0; i<length; i++) {
+        std::cout << arr[i] << "\t";
+    }
+    std::cout << "\n";
+    selectionSort(arr, length, ascending);
+    for (int i=0; i<length; i++) {
+        std::cout << arr[i] << "\t";
+    }
+
+	return 0;
+}
 
