52e3e8bd5d88e59226c6bddb9bec856e3b149477
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-04-13 01:04:16 +0700

    C++ 13/04/2022

---
+++ 11_Keo_Bua_Bao.c
@@ -0,0 +1,18 @@
+
+/*
+    * Viết trò chơi bao - đá - kéo với luật chơi: bao thắng đá, đá thắng kéo, kéo
+    thắng bao. Người dùng nhập vào một trong ba ký tự b (bao), d (đá), k (kéo); máy
+    tính sinh ngẫu nhiên một trong ba ký tự trên, thông báo kết quả chơi.
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main() {
+    char humun, computer;
+
+
+    return 0;
+}
