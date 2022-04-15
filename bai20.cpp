102236cf6e15193ea345ff366ab0767f68cc613a
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-04-15 15:26:19 +0700

    C++ 15/04/2022

---
+++ bai20.cpp
@@ -0,0 +1,32 @@
+
+
+/*
+    Viết một chương trình nhập số KW điện tiêu thụ. Tính tiền điện phải trả
+    biết rằng: khung giá tiền điện như sau:
+    0kW         100kW       250kW       350kW
+    500đ/kW     800đ/kW     1000đ/kW    1500đ/kW    
+
+*/
+
+#include<iostream>
+#include<cmath>
+
+int main() {
+    int sodien, thanhtien;
+    std:cout << "Nhap vao so dien tieu thu: ";
+    std::cin >> sodien;
+    
+    if (sodien <= 100) {
+        thanhtien = sodien * 500;
+    } else if (sodien <= 250) {
+        thanhtien = sodien * 800;
+    } else if (sodien <= 350) {
+        thanhtien = sodien * 1000;
+    } else {
+        thanhtien = sodien * 1500;
+    }
+
+    std::cout << "\nSo tien phai tra: " << thanhtien << sttd::endl;
+
+    return 0;
+}
---
+++ bai21.cpp
@@ -0,0 +1,78 @@
+
+/*
+    Trong một kỳ tuyển sinh, một thí sinh sẽ trung tuyển nếu có điểm tổng kết lớn hơn hoặc bằng điển chuẩn
+    và không có môn nào điểm 0
+    + điểm tổng kết là tổng điểm của 3 môn thi và điểm ưu tiên
+    + điểm ưu tiên bao gồm: điểm ưu tiên theo khu vực và điểm ưu tiên theo đối tượng
+  
+    Khu vực                         Đối tượng
+    A       B       C               1       2       3
+    2       1       0.5             2.5     1.5     1
+
+*/
+
+#include<iostream>
+#include<cmath>
+
+struct DiemChuan {
+    float diemchuan; 
+    float diemtoan, diemly, diemhoa;
+    char KV;
+    int doituong;
+
+};
+
+int main() {
+    struct DiemChuan p1;
+
+    do {
+        
+        std::cout << "\nNhap vao diem chuan: ";
+        std::cin >> p1.diemchuan;    
+        std::cout << "Nhap vao diem 3 mon(Toan - Ly - Hoa): ";
+        std::cin >> p1.diemtoan >> p1.diemly >> p1.diemhoa;
+        if (p1.diemchuan > 30 || p1.diemtoan > 10 || p1.diemly > 10 || p1.diemhoa > 10) {
+            std::cout << "\nNhap Loi, Nhap lai: ";
+        }
+        
+    } while (p1.diemchuan > 30 || p1.diemtoan > 10 || p1.diemly > 10 || p1.diemhoa > 10);
+
+    std::cout << "Nhap vao khu vuc(A - B - C - X): ";
+    std::cin >> p1.KV;
+    std::cout << "Nhap vao doi tuong(1 - 2 - 3 - 0): ";
+    std::cin >> p1.doituong;
+
+    float DiemTong;
+
+    switch(p1.KV) {
+        case 'A': DiemTong = 2; break;
+        case 'B': DiemTong = 1; break;
+        case 'C': DiemTong = 0.5; break;
+        case 'X': DiemTong = 0; break;
+        default: DiemTong = 0; break;
+    }
+
+
+    if (p1.doituong == 1) {
+        DiemTong += p1.diemtoan + p1.diemly + p1.diemhoa + 2.5;
+    } else if (p1.doituong == 2) {
+        DiemTong += p1.diemtoan + p1.diemly + p1.diemhoa + 1.5;
+    } else if (p1.doituong == 3){
+        DiemTong += p1.diemtoan + p1.diemly + p1.diemhoa + 1;
+    } else {
+        DiemTong += p1.diemtoan + p1.diemly + p1.diemhoa;
+    }
+
+
+    if (DiemTong < p1.diemchuan) {
+        std::cout << "\nRot - " << DiemTong << std::endl;
+    } else {
+        std::cout << "\nDau - " << DiemTong << std::endl;
+    }
+
+
+
+
+    return 0;
+}
+
