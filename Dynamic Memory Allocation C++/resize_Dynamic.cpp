892061667ba5f2e4d1fc19ef985f9f78a3e4c189
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-05-06 23:06:17 +0700

    C++ Dynamic

---
+++ Dynamic Memory Allocation C++/resize_Dynamic.cpp
@@ -0,0 +1,61 @@
+
+
+// THAY ĐỔI KÍCH THƯỚC KHI CẤP PHÁT ĐỘNG TRONG C++
+
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<bits/stdc++.h>
+using namespace std;
+
+//! Cách 1:
+void resize(int *&arr, int newsize) {
+    delete[] arr;
+    arr = new int[newsize];
+}
+
+int main() {
+    int *arr = new int[10];
+    resize(arr, 1000);
+    for(int i=0; i<1000; i++) {
+        arr[i] = i;
+    }
+    for(int i = 0; i < 1000; i++) {
+        cout << arr[i] << " ";
+    }
+    delete[] arr;
+    return 0;
+}
+
+
+
+
+
+
+
+
+//!  Cách 2:
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<bits/stdc++.h>
+using namespace std;
+
+// Cách 2:
+void resize(int **arr, int newsize) {
+    delete[] *arr;
+    *arr = new int[newsize];
+}
+
+int main() {
+    int *arr = new int[10];
+    resize(&arr, 1000);
+    for(int i=0; i<1000; i++) {
+        arr[i] = i;
+    }
+    for(int i = 0; i < 1000; i++) {
+        cout << arr[i] << " ";
+    }
+    delete[] arr;
+    return 0;
+}
