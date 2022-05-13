7abd9fef8f72db89d1bf9e9ac778bfd20797e230
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-05-13 23:35:31 +0700

    C++ 13/05/2022

---
+++ C++ STL Tutorial/Set/10_find.cpp
@@ -0,0 +1,24 @@
+
+/*  C++ set find():
+
+    C++ set find() function is used to find an element with the given value val. 
+    If it finds the element then it returns an iterator pointing to the element otherwise, 
+    it returns an iterator pointing to the end of the set i.e. set::end().
+
+
+*/
+
+#include <iostream>  
+#include <set>  
+  
+using namespace std;  
+  
+int main(void) {  
+    set<int> m = {100,200,300,400};  
+    
+    auto it = m.find(300);  
+    
+    cout << "Iterator points to " << *it << endl;  
+    
+    return 0;  
+}  
---
+++ C++ STL Tutorial/Set/11_count.cpp
@@ -0,0 +1,39 @@
+
+/*  C++ set count():
+
+
+
+
+*/
+
+#include <iostream>  
+#include <set>  
+  
+using namespace std;  
+   
+int main()  
+{  
+    // initialize container  
+    set<int> mp;  
+   
+    // insert elements in random order  
+    mp.insert(30);  
+    mp.insert( 40 );  
+    mp.insert( 60 );  
+    mp.insert( 20);  
+    mp.insert( 50 );  
+   
+    // checks if key 30 is present or not  
+    if (mp.count(30))  
+        cout << "The key 30 is present\n";  
+    else  
+        cout << "The key 30 is not present\n";  
+   
+    // checks if key 100 is present or not  
+    if (mp.count(100))  
+        cout << "The key 100 is present\n";  
+    else  
+        cout << "The key 100 is not present\n";  
+   
+    return 0;  
+}  
