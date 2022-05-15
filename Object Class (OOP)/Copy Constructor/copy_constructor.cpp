3b08cad05e8b48469b48420f03f78d061a798397
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-05-15 11:19:04 +0700

    C++ 15/05/2022

--- Object Class (OOP)/Copy Constructor/copy_constructor.cpp
+++ Object Class (OOP)/Copy Constructor/copy_constructor.cpp
@@ -5,26 +5,32 @@
     một đối tượng của cùng lớp đó, mà đã được tạo trước đó. 
 
     Copy constructor được sử dụng để:
-    Khởi tạo một đối tượng từ đối tượng khác với cùng kiểu.
+        Khởi tạo một đối tượng từ đối tượng khác với cùng kiểu.
 
 
+    - COPY CONSTRUCTOR CÓ 2 LOẠI:
+        + Default Copy Constructor:
+        + User Defined constructor: The programmer defines the user-defined constructor.
 
-    Syntax Of User-defined Copy Constructor:
+
+    Syntax Of User-defined Copy Constructor:    
 
                         Class_name(const class_name &old_object);  
 
+                        class ten_lop  
+                        {  
+                            ten_lop (const ten_lop &obj) {
+                                / phan than cua constructor
+                            } 
+                        }   
 
-    class ten_lop  
-    {  
-        ten_lop (const ten_lop &obj) {
-             !phan than cua constructor
-        } 
-    }   
+    Two types of copies are produced by the constructor:
+        + Shallow copy: sao chép nông
+        + Deep copy : sao chép sâu
 
 */
 
 #include <iostream>
-
 using namespace std;
 
 class Line
@@ -77,6 +83,61 @@ int main( )
     Line line(50);
 
     hienThi(line);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//! ////////////////////////////////
+// Shallow Copy
+#include<bits/stdc++.h>
+using namespace std;
+
+class Demo {
+    public:
+        int a, b;
+        int *p;
+    public:
+        Demo() {
+            p = new int;
+        }
+        void setdata(int, int, int);
+        void showdata();
+
+};
+
+void Demo::setdata(int x, int y, int z) {
+    a = x;
+    b = y;
+    *p = z;
+}
+
+void Demo::showdata() {
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << *p << endl;
+}
+
+
+int main() {
+    Demo d1;
+    d1.setdata(12, 04, 2003);
+
+    Demo d2;
+    d2 = d1;
+    
+    d2.showdata();
+
 
     return 0;
 }
