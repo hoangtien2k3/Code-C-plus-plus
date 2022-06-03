9f9dc8462a1cc744d5492e9e121c263bf0acc9d9
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-06-03 18:23:49 +0700

    C++ 03/06/2022

---
+++ 0_Object Class (OOP)/2_Polymorphism.cpp/Difference_CompileTime_RunTime_Polymorphism.cpp
@@ -0,0 +1,34 @@
+
+
+// ! Differences b/w compile time and run time polymorphism.
+
+    - Compile Time Polymorphism:
+       
+       + The function to be invoked(gọi, biết tới) is know at the complite time.
+
+        + Overloading is a compile time polymorphism where more than one method is having the same name but with the different 
+        number of parameters or the type of the parameters.
+
+
+
+
+
+
+    - Run Time Poltmorphism:
+        +The function to be invoked(goi), í know at the run time.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
