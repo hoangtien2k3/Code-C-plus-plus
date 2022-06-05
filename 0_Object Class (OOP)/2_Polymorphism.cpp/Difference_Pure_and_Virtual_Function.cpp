8f273591b86b3cd006d63c1f28091137d44f0b6d
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-06-05 11:17:29 +0700

    C++ 05/06/2022

---
+++ 0_Object Class (OOP)/2_Polymorphism.cpp/Difference_Pure_and_Virtual_Function.cpp
@@ -0,0 +1,7 @@
+
+
+// Sự khác nhau giữa virtual function và pure virtual fuction.
+
+
+//!  Virtual Function:
+    + A virtual function is a member function in a base class that can be redefined in a derived class.
