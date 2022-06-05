6816ec6ad1d5d6131127b74bce59e807d833ff90
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-06-05 11:24:32 +0700

    C++ 05/06/2022

--- 0_Object Class (OOP)/2_Polymorphism.cpp/Difference_Pure_and_Virtual_Function.cpp
+++ 0_Object Class (OOP)/2_Polymorphism.cpp/Difference_Pure_and_Virtual_Function.cpp
@@ -1,7 +1,21 @@
 
-
-// Sự khác nhau giữa virtual function và pure virtual fuction.
-
+// Difference between Virtual Function(hàm ảo) and Pure Virtual Function(hàm thuần ảo) in Abstraction OOP C++
 
 //!  Virtual Function:
     + A virtual function is a member function in a base class that can be redefined in a derived class.
+    
+    + The classes which are containing virtual functions are not abstract classes.
+
+    + In case of a virtual function, definition of a function is provided in the base class.
+
+
+
+
+//! Pure Virtual Function:
+
+    + A pure virtual function is a member function in a base class whose declaration is provided in 
+    a base class and implemented in a derived class. 
+
+    + The classes which are containing pure virtual function are the abstract classes.
+
+    + In case of a pure virtual function, definition of a function is not provided in the base class.
